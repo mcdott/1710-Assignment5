a5d6528c315ac1355bdec5653c301405873ec7e6
mcdott <111994803+mcdott@users.noreply.github.com>
2022-11-15 19:40:04 -0500

    Completed the edit plant and the delete plant sections

--- app.py
+++ app.py
@@ -8,23 +8,11 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-# client = MongoClient('localhost', 27017)
-# db = client.plants_db
-# plants = db.plants
-# # harvests = db.harvests
-# harvests = client.harvests_db.harvests
-
 myclient = MongoClient("mongodb://localhost:27017/")
 plants_db = myclient["plants"]
 plants = plants_db["plants"]
-
 harvests_db = myclient["harvests"]
 harvests = harvests_db["harvests"]
-
-
-
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
-# mongo = PyMongo(app)
 
 ############################################################
 # ROUTES
@@ -34,8 +22,7 @@ harvests = harvests_db["harvests"]
 def plants_list():
     """Display the plants list page."""
 
-    # TODO: Replace the following line with a database call to retrieve *all*
-    # plants from the Mongo database's `plants` collection.
+    # Makes a database call to retrieve *all* plants from the Mongo database's `plants` collection.
     plants_data = plants.find()
 
     context = {
@@ -52,8 +39,8 @@ def about():
 def create():
     """Display the plant creation page & process data from the creation form."""
     if request.method == 'POST':
-        # TODO: Get the new plant's name, variety, photo, & date planted, and 
-        # store them in the object below.
+        # Gets the new plant's name, variety, photo, & date planted, and 
+        # stores them in the new_plant object.
         plant_name = request.form.get('plant_name')
         new_plant = {
             'name': plant_name,
@@ -61,9 +48,9 @@ def create():
             'photo_url': request.form.get('photo'),
             'date_planted': request.form.get('date_planted'),
         }
-        # TODO: Make an `insert_one` database call to insert the object into the
-        # database's `plants` collection, and get its inserted id. Pass the 
-        # inserted id into the redirect call below.
+        # Makes an `insert_one` database call to insert the object into the
+        # database's `plants` collection, and get its inserted id. Passes the 
+        # inserted id into the redirect call.
 
         new_plant_id = plants.insert_one(new_plant).inserted_id
 
@@ -75,20 +62,13 @@ def create():
 @app.route('/plant/<plant_id>')
 def detail(plant_id):
     """Display the plant detail page & process data from the harvest form."""
-
-    # TODO: Replace the following line with a database call to retrieve *one*
-    # plant from the database, whose id matches the id passed in via the URL.
-    
-    # print(plant_id)
+    # Makes a database call to retrieve *one* plant from the database, 
+    # whose id matches the id passed in via the URL.
     searchParams = {'_id': ObjectId(plant_id)}
     selected_plant_document = plants.find_one(searchParams)
-    print('*******')
-    print(selected_plant_document) # this prints the document correctly
 
-    # TODO: Use the `find` database operation to find all harvests for the
-    # plant's id.
-    # HINT: This query should be on the `harvests` collection, not the `plants`
-    # collection.
+    # Uses the `find` database operation to find all harvests for the plant's id.  
+    # This query is on the `harvests` collection, not the `plants` collection.
     searchParams = {'plant_id': plant_id}
     harvests_result = harvests.find(searchParams)
 
@@ -104,51 +84,56 @@ def harvest(plant_id):
     Accepts a POST request with data for 1 harvest and inserts into database.
     """
 
-    # TODO: Create a new harvest object by passing in the form data from the
+    # Creates a new harvest object by passing in the form data from the
     # detail page form.
     new_harvest = {
         'quantity': request.form.get('harvested_amount'), # e.g. '3 tomatoes'
         'date': request.form.get('date_harvested'),
         'plant_id': plant_id,
-        # '_id': ObjectId(plant_id) this causes duplicate key error for id
     }
 
-    # TODO: Make an `insert_one` database call to insert the object into the 
+    # Makes an `insert_one` database call to insert the object into the 
     # `harvests` collection of the database.
 
-    new_harvest = harvests.insert_one(new_harvest).inserted_id
-    print('###')
-    print(new_harvest)
-    # harvests.insert_one(new_harvest)
+    harvests.insert_one(new_harvest)
     return redirect(url_for('detail', plant_id=plant_id))
 
 @app.route('/edit/<plant_id>', methods=['GET', 'POST'])
 def edit(plant_id):
     """Shows the edit page and accepts a POST request with edited data."""
     if request.method == 'POST':
-        # TODO: Make an `update_one` database call to update the plant with the
+        # Makes an `update_one` database call to update the plant with the
         # given id. Make sure to put the updated fields in the `$set` object.
-
+        searchParams = {'_id': ObjectId(plant_id)}
+        updated_values = { '$set':
+            {'name': request.form.get('plant_name'),
+            'variety': request.form.get('variety'),
+            'photo_url': request.form.get('photo'),
+            'date_planted': request.form.get('date_planted')},
+        }
+        plants.update_one(searchParams, updated_values)
         
         return redirect(url_for('detail', plant_id=plant_id))
     else:
-        # TODO: Make a `find_one` database call to get the plant object with the
-        # passed-in _id.
-        plant_to_show = ''
+        # Makes a `find_one` database call to get the plant object with the passed-in _id.
+        searchParams = {'_id': ObjectId(plant_id)}
+        plant_to_show = plants.find_one(searchParams)
 
         context = {
             'plant': plant_to_show
         }
-
         return render_template('edit.html', **context)
 
 @app.route('/delete/<plant_id>', methods=['POST'])
 def delete(plant_id):
-    # TODO: Make a `delete_one` database call to delete the plant with the given
-    # id.
+    """Deletes the plant whose id was passed in."""
+    # Makes a `delete_one` database call to delete the plant with the given id.
+    searchParams = {'_id': ObjectId(plant_id)}
+    plants.delete_one(searchParams)
 
-    # TODO: Also, make a `delete_many` database call to delete all harvests with
-    # the given plant id.
+    # Makes a `delete_many` database call to delete all harvests with the given plant id.
+    searchParams = {'plant_id': plant_id}
+    harvests.delete_many(searchParams)
 
     return redirect(url_for('plants_list'))
 
